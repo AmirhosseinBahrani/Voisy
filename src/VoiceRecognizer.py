24dc3384750fb153ba460a3f177b1e10307a9bb1
AmirhosseinBA <77787165+AmirhosseinBA@users.noreply.github.com>
2021-10-13 09:27:44 +0330

    Time and resizing audio file has been added

--- src/VoiceRecognizer.py
+++ src/VoiceRecognizer.py
@@ -1,15 +1,28 @@
 import speech_recognition as sr
 import time
+from FileResizer import FileResizer
+import math
+from pydub import AudioSegment
+
 r = sr.Recognizer()
-def recognize(source):
-   return r.recognize_google(audio_text)
-def fileToText(name):
-   audioFile = sr.audioFile(r + "./" + "wav")
-   with audioFile as sound:
-      text = r.record(sound)
-   return text
-def listen():
-   with sr.Microphone() as sound:
-      print("speak")
-      audio = r.listen(sound)
-      return audio
+file_audio = sr.AudioFile('/src/saq.wav')
+time_to_start = 0
+first_start = time.time()
+
+while True:
+   start = time.time()
+   try:
+      #r.adjust_for_ambient_noise(source)
+      with file_audio as source:
+         audio_text = r.listen(source,0.7)
+         print(r.recognize_google(audio_text, language="en-US", show_all=False))
+   except sr.WaitTimeoutError:
+      print("Ended")
+      end = time.time()
+      time_to_start = abs(start - end) * 100
+      print(abs(time_to_start * 100))
+      t1 = time_to_start * 1000 #Works in milliseconds
+      FR = FileResizer("saq.wav",t1).extract()
+      FR.extract()
+      # Update Audio file
+      file_audio = sr.AudioFile('saq.wav')

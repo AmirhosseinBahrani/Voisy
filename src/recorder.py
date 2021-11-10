04564468bdeef1978973b52022b8922caa549877
mohaka1386 <72750461+mohaka1386@users.noreply.github.com>
2021-11-10 09:13:50 +0330

    updating
    making it a class , recording in mp3 format , exporting in records folder

--- src/recorder.py
+++ src/recorder.py
@@ -2,9 +2,11 @@ import sounddevice as sd
 from scipy.io.wavfile import write 
 import os 
 
-def record(name , duration):
-    freq = 48000
-    address = ".\\audio\\wav\\" + name + ".wav"
-    recording = sd.rec(int(duration * freq), samplerate=freq ,channels=2) 
-    sd.wait() 
-    write(address , freq , recording)
+class Record:
+    def record(self , name , duration):
+        freq = 48000
+        address = ".\\audio\\records\\" + name + ".mp3"
+        recording = sd.rec(int(duration * freq), samplerate=freq ,channels=2) 
+        sd.wait() 
+        open(address , "x")
+        write(address , freq , recording)
